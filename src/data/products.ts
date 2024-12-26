125d9a35b1000b10f4e62f9127c247cf6c21494c
trungtran <baotrung06092003@gmail.com>
2024-12-26 22:04:19 +0700

    update mockdata

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,20 +1,21 @@
 export const productList = [
   {
     id: 1,
-    name: 'Something',
+    name: 'Brewed Light',
     price: '335000',
     previewImage:
       'https://i.pinimg.com/736x/97/8f/27/978f2763395e2bea8a9b298b6933af10.jpg',
     images: [
       'https://i.pinimg.com/736x/97/8f/27/978f2763395e2bea8a9b298b6933af10.jpg',
-      'https://i.pinimg.com/736x/f4/29/8f/f4298f941a036123b09d8a7378089673.jpg',
+      'https://i.pinimg.com/736x/85/98/4f/85984f9cfdadf9022fbc378c55cee777.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
   {
     id: 2,
-    name: 'Something',
+    name: 'Grounded Glow',
     price: '140000',
     previewImage:
       'https://i.etsystatic.com/20263210/r/il/358d95/6209064303/il_794xN.6209064303_49qp.jpg',
@@ -22,12 +23,13 @@ export const productList = [
       'https://i.etsystatic.com/20263210/r/il/42fe3c/6160999732/il_794xN.6160999732_4a7u.jpg',
       'https://i.etsystatic.com/20263210/r/il/cafc66/6160999234/il_794xN.6160999234_nbmf.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
   {
     id: 3,
-    name: 'Something',
+    name: 'Roasted Glow',
     price: '225000',
     previewImage:
       'https://i.etsystatic.com/29037752/r/il/d66ce2/5499963652/il_794xN.5499963652_1blg.jpg',
@@ -35,12 +37,13 @@ export const productList = [
       'https://i.etsystatic.com/29037752/r/il/d66ce2/5499963652/il_794xN.5499963652_1blg.jpg',
       'https://i.etsystatic.com/29037752/r/il/12bb6e/5520627081/il_794xN.5520627081_go3u.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
   {
     id: 4,
-    name: 'Coffee Mocha',
+    name: 'Java Candle',
     price: '450000',
     previewImage:
       'https://i.etsystatic.com/10813860/r/il/9e7bf9/5688709655/il_794xN.5688709655_i20j.jpg',
@@ -48,12 +51,13 @@ export const productList = [
       'https://i.etsystatic.com/10813860/r/il/9e7bf9/5688709655/il_794xN.5688709655_i20j.jpg',
       'https://i.etsystatic.com/10813860/r/il/d9c766/5688711837/il_794xN.5688711837_mhkr.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
   {
     id: 5,
-    name: 'Coffee Mocha',
+    name: 'Brew Cup',
     price: '100000',
     previewImage:
       'https://biopolymer.vn/wp-content/uploads/2022/09/muggggggg-1080x1080.jpg',
@@ -61,12 +65,13 @@ export const productList = [
       'https://biopolymer.vn/wp-content/uploads/2022/09/muggggggg-1080x1080.jpg',
       'https://product.hstatic.net/1000361058/product/111111_218d4989fece4d4ea97bc868336d767f_master.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'glass',
   },
   {
     id: 6,
-    name: 'Coffee Mocha',
+    name: 'EarthBrew Cup',
     price: '100000',
     previewImage:
       'https://cdn.tuoitre.vn/2022/12/13/togo-3-16709189160382129296956.png',
@@ -74,12 +79,13 @@ export const productList = [
       'https://cdn.tuoitre.vn/2022/12/13/togo-3-16709189160382129296956.png',
       'https://biopolymer.vn/wp-content/uploads/2022/05/Shoex-0911-___6024-scaled.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'glass',
   },
   {
     id: 7,
-    name: 'Coffee Mocha',
+    name: 'Java Utensils',
     price: '100000',
     previewImage:
       'https://bizweb.dktcdn.net/thumb/grande/100/384/391/articles/svsav.jpg?v=1676784533357',
@@ -87,7 +93,8 @@ export const productList = [
       'https://bizweb.dktcdn.net/thumb/grande/100/384/391/articles/svsav.jpg?v=1676784533357',
       'https://cdn.tgdd.vn/Files/2017/07/02/998938/cach-lam-tui-thom-ca-phe-tai-nha2_800x400.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'sachet',
   },
 ];
@@ -95,7 +102,7 @@ export const productList = [
 export const productHighlights = [
   {
     id: 1,
-    name: 'Something',
+    name: 'Brewed Light',
     price: '335000',
     previewImage:
       'https://i.pinimg.com/736x/97/8f/27/978f2763395e2bea8a9b298b6933af10.jpg',
@@ -103,12 +110,13 @@ export const productHighlights = [
       'https://i.pinimg.com/736x/97/8f/27/978f2763395e2bea8a9b298b6933af10.jpg',
       'https://i.pinimg.com/736x/f4/29/8f/f4298f941a036123b09d8a7378089673.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
   {
     id: 2,
-    name: 'Something',
+    name: 'Grounded Glow',
     price: '140000',
     previewImage:
       'https://i.etsystatic.com/20263210/r/il/358d95/6209064303/il_794xN.6209064303_49qp.jpg',
@@ -116,12 +124,13 @@ export const productHighlights = [
       'https://i.etsystatic.com/20263210/r/il/42fe3c/6160999732/il_794xN.6160999732_4a7u.jpg',
       'https://i.etsystatic.com/20263210/r/il/cafc66/6160999234/il_794xN.6160999234_nbmf.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
   {
     id: 3,
-    name: 'Something',
+    name: 'Roasted Glow',
     price: '225000',
     previewImage:
       'https://i.etsystatic.com/29037752/r/il/d66ce2/5499963652/il_794xN.5499963652_1blg.jpg',
@@ -129,12 +138,13 @@ export const productHighlights = [
       'https://i.etsystatic.com/29037752/r/il/d66ce2/5499963652/il_794xN.5499963652_1blg.jpg',
       'https://i.etsystatic.com/29037752/r/il/12bb6e/5520627081/il_794xN.5520627081_go3u.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
   {
     id: 4,
-    name: 'Coffee Mocha',
+    name: 'Java Candle',
     price: '450000',
     previewImage:
       'https://i.etsystatic.com/10813860/r/il/9e7bf9/5688709655/il_794xN.5688709655_i20j.jpg',
@@ -142,7 +152,8 @@ export const productHighlights = [
       'https://i.etsystatic.com/10813860/r/il/9e7bf9/5688709655/il_794xN.5688709655_i20j.jpg',
       'https://i.etsystatic.com/10813860/r/il/d9c766/5688711837/il_794xN.5688711837_mhkr.jpg',
     ],
-    description: 'This is a description',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     type: 'candle',
   },
 ];
